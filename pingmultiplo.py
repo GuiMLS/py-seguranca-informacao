c7ae4049c251cde1b8778962fd39469dd3534cdd
Guilherme Sousa <guilherme.sousa@engdb.com.br>
2023-03-10 17:01:02 -0300

    Adicao da biblioteca time com sleep entre pings

--- pingmultiplo.py
+++ pingmultiplo.py
@@ -1,17 +1,18 @@
 import os  # Importa módulo os, que usa recursos nativos do SO
-# import time
-import re
+import re  # Regex
+import time
+
 lista_corrigida = []
 
 
 def pingar(ip_a_pingar):
-    print(os.system(f'ping -n 6 {ip_a_pingar}'))
+    print(os.system(f'ping -n 2 {ip_a_pingar}'))
+    time.sleep(2)
 
 
 with open('hosts.txt') as file:
     dump = file.read()
 chunks = dump.split('\n')
-
 for elemento in chunks:
     pat = re.compile("\S+\.\S+\.")  # Regex pega apenas itens que são IP/Host.
     test = pat.match(elemento)
@@ -20,4 +21,3 @@ for elemento in chunks:
         pingar(elemento)
     else:
         print(f"{elemento} não me parece um IP/host. Conserte esse endereço!")
-# print(lista_corrigida)
