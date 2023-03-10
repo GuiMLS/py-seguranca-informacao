811b5544a3e572a3d45e54a75160d52f32321014
Guilherme Sousa <guilherme.sousa@engdb.com.br>
2023-03-10 16:27:17 -0300

    Versao generica e com validação com endereço.

--- pingmultiplo.py
+++ pingmultiplo.py
@@ -1,7 +1,23 @@
 import os  # Importa módulo os, que usa recursos nativos do SO
-import time
+# import time
+import re
+lista_corrigida = []
 
 
-ip_ou_host = input("Digite o IP ou Host a ser verificado: ")
-print(f"Pingando o IP/Host {ip_ou_host}:")
-os.system(f'ping -n 6 {ip_ou_host}')
+def pingar(ip_a_pingar):
+    print(os.system(f'ping -n 6 {ip_a_pingar}'))
+
+
+with open('hosts.txt') as file:
+    dump = file.read()
+chunks = dump.split('\n')
+
+for elemento in chunks:
+    pat = re.compile("\S+\.\S+\.")  # Regex pega apenas itens que são IP/Host.
+    test = pat.match(elemento)
+    if test:
+        lista_corrigida.append(elemento)
+        pingar(elemento)
+    else:
+        print(f"{elemento} não me parece um IP/host. Conserte esse endereço!")
+# print(lista_corrigida)
