d090b364c3a43a3ee1032023af40ac3a1a2248f2
Guilherme Sousa <guilherme.sousa96@edu.pucrs.br>
2023-03-12 14:32:52 -0300

    Inputs para tornar o programa generico

--- gerador-senhas.py
+++ gerador-senhas.py
@@ -1,10 +1,10 @@
 import random
 import string
 
-tamanho = 16
+tamanho = int(input("Digite quantos caracteres terá a senha a ser gerada: "))
 
 chars = string.ascii_letters + string.digits + '!çÇ@#%$&*()-=+,.;:/?'
 
 rnd = random.SystemRandom() # os.urandom
-
+print("Sua senha gerada é:")
 print(''.join(rnd.choice(chars) for i in range(tamanho)))
